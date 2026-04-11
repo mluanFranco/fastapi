ee9a70541143fe4e9ea9180232d1357bc8241cdc
mluanFranco <matheus.franco1712@gmail.com>
2026-04-11 11:46:44 -0300

    aula01

---
+++ main.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Para rodar o código, executar no terminal: uvicorn main:app --reload (Criar um servidor com uvicorn, utilizando a variável app do arquivo main)
